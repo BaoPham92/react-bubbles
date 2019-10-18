05adb101a5b288003e091696b18a25ea7b0314ea
GUL-Bao <BaoPham92@yahoo.com>
2019-10-18 11:19:22 -0400

    Initial, axiosWithAuth, and TODO created

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  useEffect(() => {
+    // TODO: CREATE AXIOS CALL HERE FOR TOKEN IF CREDENTIALS ARE CORRECT!
+    console.log('LOGIN FORM')
+  }, [])
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
